2ba38d01bd65895d4b88824bc0db9d91d0f494ad
Lunarprom <haiman20203@gmail.com>
2015-02-22 17:30:50 -0800

    第五题两张图都在同一个程序里画的

--- proj2_5_correlation_plot.py
+++ proj2_5_correlation_plot.py
@@ -6,35 +6,38 @@ Created on Sat Feb 21 12:36:05 2015
 """
 
 import matplotlib.pyplot as plt
+import datetime,time
 
 ##Extract the rate
-trate1=[]
-trate2=[]
-trate=[]
 f=open('first_second_correlation.txt','r')
+rate1=[]
+rate2=[]
+rate=[]
+elapse=[]
+
 for line in f:
     data=line.split()
-    trate1.append(data[2])
-    trate2.append(data[4])
-    trate.append(data[6])
-
-rate1=map(float,trate1)
-rate2=map(float,trate2)
-rate=map(float,trate)
+    rate1.append(float(data[2]))
+    rate2.append(float(data[4]))
+    rate.append(float(data[6]))
+    timefrom=time.strftime("%H:%M:%S",time.localtime(float(data[0])))
+    elapse.append(timefrom)
 
     
 ##The first pic
-plt.figure(1) # 创建图表1
+plt.figure(1)
 plt.xlabel('Tweeting rate of #Patriots')
 plt.ylabel('Tweeting rate of #SuperBowlXLIX')
 plt.title('The Correlation Between The Tags')
 plt.plot(rate2,rate1,'o')
 
 ##The second one
-plt.figure(2)  # 创建图表2
+plt.figure(2)
+ax=plt.subplot(111)
 plt.xlabel('Time')
 plt.ylabel('The Tweeting Rate of Tag1/Tag2')
 plt.title('The Correlation Between The Tags Over Time')
+ax.set_xticklabels(elapse,rotation=20, rotation_mode="anchor", ha="right")
 plt.plot(rate)
 
 plt.show()
